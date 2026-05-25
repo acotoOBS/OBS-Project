34d47ecd2b0d647697f56cd1d386970ffcdba102
Susana Diez Gomez <susy10g@hotmail.com>
2026-05-24 20:33:17 -0500

    fix: Ajuste order test

--- tests/test_order.py
+++ tests/test_order.py
@@ -10,33 +10,24 @@ def test_health():
 
 
 def test_get_orders_by_price_range(client):
-    # Crear órdenes
     client.post("/orders/", json={
         "customer_name": "Ana",
-        "product_name": "Laptop",
-        "quantity": 1,
+        "destination": "Bogotá",
         "price": 1000
     })
 
     client.post("/orders/", json={
         "customer_name": "Luis",
-        "product_name": "Mouse",
-        "quantity": 2,
+        "destination": "Medellín",
         "price": 50
     })
 
     response = client.get("/orders/filter/by-price?min_price=10&max_price=200")
 
-    # Validar respuesta exitosa
     assert response.status_code == 200
 
     data = response.json()
 
-    # Solo debe retornar la orden de Luis (precio 50, dentro del rango)
     assert len(data) == 1
     assert data[0]["customer_name"] == "Luis"
     assert data[0]["price"] == 50
-
-    # La orden de Ana (precio 1000) no debe aparecer
-    customer_names = [order["customer_name"] for order in data]
-    assert "Ana" not in customer_names
